a12214687c24f6c9bd3f7b6a267db02d6165a722
Osterloh <johnatanosterloh@gmail.com>
2021-11-25 16:44:13 -0300

    Projeto ReactJS

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Dashboard: React.FC = () => {
+    return (
+        <h3>Página Dashboard</h3>
+    );
+}
+
+export default Dashboard;
---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Repository: React.FC = () => {
+    return (
+        <h3>Página Repository</h3>
+    );
+}
+
+export default Repository;
