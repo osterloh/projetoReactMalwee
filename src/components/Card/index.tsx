f5c36a9e4c6943f6cc8735947c44fa3c06ddc52c
Osterloh <johnatanosterloh@gmail.com>
2021-12-02 16:37:47 -0300

    Configuração de teste

---
+++ src/components/Card/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Card: React.FC = () => {
+    return (
+        <>
+            <h3>Card</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi esse atque numquam enim libero hic officia sit, quibusdam, temporibus dicta, incidunt rerum aperiam assumenda quo! Fugit sapiente architecto expedita dolorem.</p>
+        </>
+    );
+}
+
+export default Card;
--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Card from '../../components/Card';
 
 const Repository: React.FC = () => {
     return (
-        <h3>Página Repository</h3>
+        <>
+            <h3>Página Repository</h3>
+            <Link to={`/`}>Dasboard</Link>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+        </>
     );
 }
 
