48cc70fb1fdaaa360123cda64193bedd90be64bd
Osterloh <johnatanosterloh@gmail.com>
2021-12-02 16:36:04 -0300

    Import do GlobalStyle e configuração de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
+import GlobaStyle from './styles/global';
 import Routes from './routes';
 
 const App: React.FC = () => {
   return (
-    <Routes />
+    <>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+      <GlobaStyle />
+    </>
   );
 }
 
